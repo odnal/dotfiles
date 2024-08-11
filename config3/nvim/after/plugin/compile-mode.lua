12c5c62f25c643eb6068843f75d8c67f4ab0c102
landonbr <landonbrown0797@gmail.com>
2024-08-11 00:47:49 -0400

    compile mode for vim

--- config3/nvim/after/plugin/compile-mode.lua
+++ config3/nvim/after/plugin/compile-mode.lua
@@ -1,10 +1,12 @@
-require("compile-mode").setup({
+local builtin = require("compile-mode")
+builtin.setup({
     config = function()
       vim.g.compile_mode = {
         same_window_errors = true,
         baleia_setup = true,
         default_command = "make -k",
       }
-    end
+    end,
+    --vim.keymap.set('n', '<leader>rr', "<cmd>Recompile<cr>", { desc = "Recompile" })
 })
 
--- config3/nvim/lua/odnal/remap.lua
+++ config3/nvim/lua/odnal/remap.lua
@@ -4,16 +4,29 @@ vim.keymap.set("i", "jj", "<Esc>")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "<leader>t", [[:12split term://bash]], {})
-vim.keymap.set('t', '<escape><escape>', '<C-\\><C-n>', {})
+vim.keymap.set('t', '<escape><escape>', '<C-\\><C-n>', {desc = "Escapes from terminal mode"})
 vim.keymap.set('n', '<space>a', [[:tabnew]], {})
 vim.keymap.set('n', '<space>q', [[:tabclose]], {})
 vim.keymap.set('n', '<tab>', [[:tabnext]], {})
 vim.keymap.set('n', '<space><tab>', [[:tabnext -]], {})
-vim.keymap.set('v', '<leader>y', '"+y', {}) -- E.g: <leader>yy will yank current line to os clipboard
+vim.keymap.set('v', '<leader>y', '"+y', {desc = "Yank to clipboard"})
 vim.keymap.set('v', '<leader>Y', '"+y$', {})
 vim.keymap.set('n', '<space>m', [[:Man ]], {})
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+local function with_count(cmd, default)
+  return function()
+    local default_count = type(default) == "function" and default() or default
+    local count = vim.v.count == 0 and default_count or vim.v.count
+    return "<cmd>" .. count .. cmd .. "<cr>"
+  end
+end
+vim.keymap.set('n', '<leader>cc', "<cmd>Compile<cr>", { desc = "Compile" })
+vim.keymap.set('n', '<leader>rr', "<cmd>Recompile<cr>", { desc = "Recompile" })
+vim.keymap.set("n", "<leader>p", with_count("PrevError", 1), { expr = true, desc = "Previous Error" })
+vim.keymap.set("n", "<leader>n", with_count("NextError", 1), { expr = true, desc = "Next Error" })
+
 --vim.keymap.set("n", "J", "mzJ`z")
 
 
