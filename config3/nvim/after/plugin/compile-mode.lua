9c91e2bf0a3c6762488f60ddfae725fc6161ea0a
landonbr <landonbrown0797@gmail.com>
2024-09-12 19:54:08 -0400

    update

--- config3/nvim/after/plugin/compile-mode.lua
+++ config3/nvim/after/plugin/compile-mode.lua
@@ -1,12 +1,5 @@
-local builtin = require("compile-mode")
-builtin.setup({
-    config = function()
-      vim.g.compile_mode = {
-        same_window_errors = true,
-        baleia_setup = true,
-        default_command = "make -k",
-      }
-    end,
-    --vim.keymap.set('n', '<leader>rr', "<cmd>Recompile<cr>", { desc = "Recompile" })
-})
-
+vim.g.compile_mode = {
+    --same_window_errors = true,
+    baleia_setup = true,
+    default_command = "make -k",
+}
